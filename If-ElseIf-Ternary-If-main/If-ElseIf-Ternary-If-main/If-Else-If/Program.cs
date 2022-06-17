9cd2c6ed5a111aae11ed3c53ab76a7be4ac4b1bf
Abdulkadir ÇAPUR <86108672+kadircapur@users.noreply.github.com>
2022-06-17 14:16:54 +0300

    Add files via upload

---
+++ If-ElseIf-Ternary-If-main/If-ElseIf-Ternary-If-main/If-Else-If/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace If_Else_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Gunaydin!");
+            else if (time <= 18)
+                Console.WriteLine("Iyi Gunler!");
+            else
+                Console.WriteLine("Iyi Geceler!");
+
+            string sonuc = time <= 18 ? "Iyi Gunler" : "Iyi Geceler";
+
+            sonuc = time >= 6 && time < 11 ? "Gunaydin" : time <=18 ? "Iyi Gunlar" : "Iyi Geceler";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
---
+++ If-ElseIf-Ternary-If-main/If-ElseIf-Ternary-If-main/README.md
@@ -0,0 +1,6 @@
+# If-ElseIf-Ternary-If
+Patika > C# 101 > If-ElseIf-Ternary-If
+
+![16](https://user-images.githubusercontent.com/86108672/172880499-e9466e52-0fa5-46d0-a4d5-7548065c3f3f.PNG)
+
+www.Patika.dev

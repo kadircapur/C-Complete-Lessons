9a53150c8be36ca406471c2551ad1fbaae8289ad
Abdulkadir ÇAPUR <86108672+kadircapur@users.noreply.github.com>
2022-06-17 14:17:21 +0300

    Add files via upload

---
+++ Hata_Yonetimi-main/Hata_Yonetimi-main/HataYonetımı/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HataYonetımı
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayi Giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmis oldugunuz sayi:" + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata:" + ex.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine("Islem Tamamlandi:");
+            }
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Bos Deger Girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri Tipi Uygun Degil");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Cok kucuk ya da cok buyuk bir deger girdiniz");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Islem Basariyla Tamamlandi");
+            }
+            }
+        }
+}
---
+++ Hata_Yonetimi-main/Hata_Yonetimi-main/README.md
@@ -0,0 +1,6 @@
+# Hata_Yonetimi
+Patika > C# 101 > Try Catch Finally ve Mantıksal Hatalar
+
+![image](https://user-images.githubusercontent.com/86108672/172858811-fdc8a0d0-78d2-4284-a31b-c90b7d716ca5.png)
+
+www.Patika.dev

e8f4ec06b8836d82c5c37b61c1933833a2a3cfd2
Abdulkadir ÇAPUR <86108672+kadircapur@users.noreply.github.com>
2022-06-17 14:16:29 +0300

    Add files via upload

---
+++ Operatorler-main/Operatorler-main/Operatorlerr/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Operatorlerr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 3;
+            y = y + 2;
+            Console.WriteLine(y);
+
+            y += 2;
+            Console.WriteLine(y);
+
+            y /= 1;
+            Console.WriteLine(y);
+
+            x *= 2;
+            Console.WriteLine(x);
+
+            // Mantiksal Operatorler
+            // || && !
+
+            bool isSuccess = true;
+            bool isCompleted = false;
+
+            if (isSuccess && isCompleted)           
+                Console.WriteLine("Perfect!");
+            
+            if (isSuccess || isCompleted)
+                Console.WriteLine("Great!");
+            
+            if (isSuccess && !isCompleted)
+                Console.WriteLine("Fine!");
+
+            // iliskisel Operatorler
+            // < > <= >= == !=
+
+            int a = 3;
+            int b = 4;
+            bool sonuc = a < b;
+
+            Console.WriteLine(sonuc);
+
+            sonuc = a > b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a >= b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a <= b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a == b;
+            Console.WriteLine(sonuc);
+
+            sonuc = a != b;
+            Console.WriteLine(sonuc);
+
+            //Aritmetik Operatorler
+
+            // / * + -
+
+            int sayi1 = 10;
+            int sayi2 = 5;
+            int sonuc1 = sayi1 / sayi2;
+
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+
+            sonuc1 =sayi1++;
+            Console.WriteLine(sayi1);
+
+            // % mod alir
+
+            int sonuc2 = 20 % 3;
+            Console.WriteLine(sonuc2);
+
+
+        }
+    }
+}
---
+++ Operatorler-main/Operatorler-main/README.md
@@ -0,0 +1,9 @@
+# Operatorler
+Patika > C# 101 > Operatorler
+
+Operatorler dersi uygulama goruntuleri
+
+![12](https://user-images.githubusercontent.com/86108672/172836511-f2c332d8-c5e2-4108-9d4c-f734f9e06d08.PNG)
+![13](https://user-images.githubusercontent.com/86108672/172836532-d9bf9e36-3e73-44c9-b982-4bb3504266f5.PNG)
+
+www.patika.dev

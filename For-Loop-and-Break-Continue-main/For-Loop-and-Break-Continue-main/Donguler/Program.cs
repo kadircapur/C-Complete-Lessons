83e131fbcfc907a2b49e7c101ea0aafb33f1a8b7
Abdulkadir ÇAPUR <86108672+kadircapur@users.noreply.github.com>
2022-06-17 14:17:48 +0300

    Add files via upload

---
+++ For-Loop-and-Break-Continue-main/For-Loop-and-Break-Continue-main/Donguler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekrandan girilen sayiya kadar olan tek sayilari ekrana yazdir
+
+            Console.Write("Lutfen bir sayi giriniz: ");
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++) 
+            {
+                if (i%2 == 1)
+                    Console.WriteLine(i);
+            }
+
+            // 1 ile 1000 arasindaki tek ve cift sayilarin kendi iclerindeki toplamlarini ekrana yazdir
+
+            int tekToplam = 0;
+            int ciftToplam = 0;
+            for(int i = 1; i <= 1000; i++)
+            {
+                if (i % 2 == 1)
+                    tekToplam += i;
+                else
+                    ciftToplam = +i;
+            }
+
+            Console.WriteLine("Tek Toplam: " + tekToplam);
+            Console.WriteLine("Cift Toplam: " + ciftToplam);
+
+        }
+    }
+}
---
+++ For-Loop-and-Break-Continue-main/For-Loop-and-Break-Continue-main/README.md
@@ -0,0 +1,4 @@
+# For-Loop-and-Break-Continue
+Patika > C# 101 > For Loop ve Break Continue Ifadeleri
+
+www.Patika.dev
